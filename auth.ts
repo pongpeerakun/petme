c6b5a82340b876806d239559087f75384d323ccb
pongpeera00_kun <pongpeera.jpans@gmail.com>
2025-04-12 23:19:30 +0900

    Enhance authentication schema and configuration
    - Added new fields to the user table: role, banned, banReason, banExpires, username, and displayUsername.
    - Updated session table to include impersonatedBy field.
    - Integrated admin and username plugins into the authentication setup.
    - Introduced new migration script in package.json for better-auth and drizzle-kit.
    - Updated migration journal with new entries for recent changes.

--- auth.ts
+++ auth.ts
@@ -3,14 +3,15 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { drizzle } from "drizzle-orm/postgres-js"
 import { config } from "dotenv"
 import postgres from "postgres"
+import { admin, username } from "better-auth/plugins"
 
 
 config({ path: ".env" })
 const client = postgres(process.env.DATABASE_URL!)
-
 const db = drizzle({ client })
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
-    })
+    }),
+    plugins: [admin(), username()]
 })
